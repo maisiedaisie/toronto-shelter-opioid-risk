f1d7d4d22d45a827fbffb0eaa0b41f9b5cae232c
Maisie Davis <me_davis11@icloud.com>
2026-04-10 16:34:05 -0400

    Update 4-figures.R
    Added Figure 1

--- 4-figures.R
+++ 4-figures.R
@@ -1,1 +1,8 @@
-
+# Figure 1: Shelter Sites and Scaled Overdose Locations
+ggplot() +
+    geom_sf(data = neighbourhoods, fill = "white", color = "black") +
+    geom_sf(data = overdoses_sf, aes(size = OD_COUNT), color = "pink1", alpha = 0.7) +
+    geom_sf(data = shelters_sf, color = "palegreen2", shape = 4, size = 3) +
+    theme_minimal() +
+    labs(title = "Opioid Toxicity Events and Shelter Locations in Toronto, 2025",
+    size = "Number of Suspected Opioid Poisonings")
